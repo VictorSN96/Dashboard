933713d8cc63caa3bc1e9521252389db39b954f0
VictorSN96 <victornsnascimento@gmail.com>
2023-07-18 14:09:41 -0300

    feat: botao toggle drawer.  botao temas mantido

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,26 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate} from 'react-router-dom';
-import { useAppThemeContext } from '../shared/contexts';
+import { useAppThemeContext, useDrawerContext } from '../shared/contexts';
+
 
 
 
 export const Approutes = () =>{
     const { toggleTheme } = useAppThemeContext();
+    const { toggleDrawerOpen} = useDrawerContext();
 
     return(
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant='contained' color='primary' onClick={toggleTheme}> Temas </Button>} />
-
+            <Route 
+                path="/pagina-inicial" 
+                element={
+                    <div>
+                        <Button variant='contained' color='primary' onClick={toggleDrawerOpen}> Toggle Drawer </Button>
+                        <Button variant='contained' color='primary' onClick={toggleTheme}> Temas </Button>
+                    </div>
+                }
+            />
+            
             <Route path='*' element={<Navigate to="/pagina-inicial"/>} />
         </Routes>
 
