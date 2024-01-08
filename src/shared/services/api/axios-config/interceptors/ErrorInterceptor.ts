b3263bb81cc80c00a8392808ee0719242465caed
VictorSN96 <victornsnascimento@gmail.com>
2024-01-08 17:26:18 -0300

    Interceptors

---
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,15 @@
+import { AxiosError } from 'axios';
+
+export const errorInterceptor = (error: AxiosError) =>{
+
+    if(error.message === 'Network Error') {
+        return Promise.reject(new Error('Erro de conexão.'));
+    }
+
+    if(error.response?.status === 401) {
+        // Faça alguma coisa
+    }
+
+    return Promise.reject(error);
+
+};
