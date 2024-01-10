baadbe34eeb9b216c26631873258b2b7b8519c79
VictorSN96 <victornsnascimento@gmail.com>
2024-01-10 11:13:22 -0300

    feat Criando o service de pessoas

---
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -0,0 +1,21 @@
+
+
+const getAll = async (): Promise<any> =>{};
+
+const getById = async (): Promise<any> =>{};
+
+const create = async (): Promise<any> =>{};
+
+const updateById = async (): Promise<any> =>{};
+
+const deleteById = async (): Promise<any> =>{};
+
+export const PessoaService = {
+    getAll, 
+    getById, 
+    create,
+    deleteById,
+    updateById,
+
+};
+
