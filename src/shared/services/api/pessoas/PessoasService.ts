f1d13dd5ffe1f80c4c740a8ea080548fcc0e243c
VictorSN96 <victornsnascimento@gmail.com>
2024-01-15 11:54:39 -0300

    Isolando o acesso ao backend

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -1,14 +1,44 @@
 import { Environment } from "../../../environment";
 import { Api } from "../axios-config";
 
-const getAll = async (page = 1, filter = ''): Promise<any> =>{
+
+interface IListagemPessoa {
+    id: number;
+    email: string;
+    cidadeId: number;
+    nomeCompleto: string;
+}
+interface IDetalhePessoa {
+    id: number;
+    email: string;
+    cidadeId: number;
+    nomeCompleto: string;
+}
+
+type TPessoasComTotalCount ={
+    data: IListagemPessoa [];
+    totalCount: number;
+
+}
+
+const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | undefined | Error> =>{
     try{
         const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
 
-        const { data } = await Api.get( urlRelativa ); // Desestruturação: chamada para a URL relativa no Bancd de Dados //
-    }  catch (error){
-        
-    }
+        const { data, headers } = await Api.get( urlRelativa ); // Desestruturação: chamada para a URL relativa no Banco de Dados //
+    
+        if (data){
+            return{
+                data,
+                totalCount: Number(headers['x-total-count'] || Environment.LIMITE_DE_LINHAS),
+            };
+        }
+
+    } catch (error){
+        console.error(error);
+        return new Error((error as {message: string}).message || 'Erro ao listar os registros.');
+
+    }     
 };
 
 const getById = async (): Promise<any> =>{};
