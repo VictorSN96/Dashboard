a4cbdf120714f6bf1f81d01b4d66d7fb568ae252
VictorSN96 <victornsnascimento@gmail.com>
2024-01-26 20:29:50 -0300

    feat: Tela de criação e edição de registro

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,14 +1,42 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+
 import { LayoutBaseDePagina } from "../../shared/layouts";
+import { FerramentasDeDetalhe } from "../../shared/components";
 
 
 
 
 export const DetalheDePessoas: React.FC = () =>{
     const { id = 'nova' } = useParams<'id'>();
+    const navigate = useNavigate();
+
+    const handleSave = () =>{
+        console.log('Save');
+    };
+
+    const handleDelete = () =>{
+        console.log('Delete');
+    };
 
     return(
-        <LayoutBaseDePagina titulo='Detalhe de Pessoas'>
+        <LayoutBaseDePagina 
+            titulo='Detalhe de Pessoas'
+            barraDeFerramentas={
+                <FerramentasDeDetalhe
+                    textoBotaoNovo='Nova'
+                    mostrarBotaoSalvareVoltar
+                    mostrarBotaoNovo={id !=='nova'}
+                    mostrarBotaoApagar={id !=='nova'}
+
+                    aoClicarEmSalvar={handleSave}
+                    aoClicarEmNovo={()=> navigate('/pessoas/detalhe/nova')}
+                    aoClicarEmSalvareVoltar={handleSave}
+                    aoClicarEmVoltar={()=>navigate('/pessoas')}
+                    aoClicarEmApagar={handleDelete}
+
+                />
+            }
+        >
             <p>DetalheDePessoas</p>
         </LayoutBaseDePagina>
     );
